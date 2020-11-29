65ef42f9dd6ff6b5c0b7be31dc34f931a07fb228
EXfeiniao <74140792+EXfeiniao@users.noreply.github.com>
2020-11-29 22:13:55 +0800

    Update model.py

--- 未弄懂/model.py
+++ 未弄懂/model.py
@@ -14,30 +14,7 @@ class DNN(nn.Module):  # Python类
         self.input_size = input_size
         self.output_size = output_size
 
-        # 对每层的定义
-        '''
-        Linear函数：线性模块。
-        Linear(1,2) 第一个参数是input的个数或者上一层的神经元的个数
-        第二个参数是本层神经元的个数或者output的个数
-        Linear函数源代码：
-        class Linear(Module)：
-            __constants__ = ['bias']
-        def __init__(self, in_features, out_features, bias=True):
-            super(Linear, self).__init__()
-            self.in_features = in_features
-            self.out_features = out_features
-            self.weight = Parameter(torch.Tensor(out_features, in_features))
-            # Parameter函数：将一个不可训练的类型Tensor转换成可以训练的类型parameter
-            并将这个parameter绑定到这个module里面
-            if bias:
-                self.bias = Parameter(torch.Tensor(out_features))
-            else:
-                self.register_parameter('bias', None)
-            self.reset_parameters()
-        需要实现的内容：y = xA^T + b
-        通过Linear函数改变张量大小？
-        batch_size
-        '''
+        # 对每层的定义(矩阵内积)
         self.fc1 = nn.Linear(input_size, 5120)
         self.fc2 = nn.Linear(5120, 1024)
         self.fc3 = nn.Linear(1024, 64)
@@ -65,13 +42,10 @@ class DNN(nn.Module):  # Python类
         表示为：
         yi = { xi if xi >= 0 & xi/ai if xi < 0 }
         '''
+        
+        # 循环计算x，实现forward
         x = F.leaky_relu(self.fc2(x))
         x = F.leaky_relu(self.fc3(x))
         x = F.leaky_relu(self.fc4(x))
         y = F.softmax(self.fc5(x), dim=1)
-        '''
-        softmax类似给出每个值在一组值中的比例
-        si = ei/sum（ej）（其中，e为自然对数，i为输入值，且为e的指数）
-        dim 这个参数是维度
-        '''
         return y
